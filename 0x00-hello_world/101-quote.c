f106e93511892aa8a9c26b50e20712b089e5e12e
Steve <evilco30@gmail.com>
2022-12-08 15:17:42 -0800

    Adding Task 4-6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,10 @@
  *
  *Return: Always 1 (success)
  */
- 
- 
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main( int argc, char** argv ) 
-{ 
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 60); 
-return (1); 
+
+int write();
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 60);
+return (1);
 }
