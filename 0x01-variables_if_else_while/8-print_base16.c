a3bc56a0b1aeadec5248f93d6c8e0d89ac063462
Steve <evilco30@gmail.com>
2022-12-09 11:54:26 -0800

    Update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ *Return: Always 0 (success)
+ */
+
+int mai(void)
+{
+
+int x,m;
+
+for (x = 0, x < 10, x++)
+putchar(x);
+
+for (m = 'a', x < 'g', m++)
+putchar(m);
+
+putchar('\n');
+
+return (0);
+}
