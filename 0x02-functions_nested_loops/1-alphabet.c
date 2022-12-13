c6779e6c352950cd5b31d49d471de6522231d80e
Steve <evilco30@gmail.com>
2022-12-13 10:26:21 -0800

    Third task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void print_alphabet(void){
+
+int aa;
+
+for(aa='a'; aa <= 'z'; aa++)
+{
+_putchar(aa);
+}
+_putchar(n);
+
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+
+void print_alphabet_x10(void);
+
+int aa, num;
+
+num = 0
+while (num <= 10){
+for(aa='a'; aa <= 'z'; aa++)
+{
+_putchar(aa);
+}
+num++;
+}
+_putchar(n);
+
+return (0);
+}
