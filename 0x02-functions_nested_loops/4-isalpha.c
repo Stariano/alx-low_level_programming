b7cd94facdbbca659b1ff9eb56e9fe9563137476
Steve <evilco30@gmail.com>
2022-12-13 10:49:51 -0800

    Third task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
 {
@@ -16,4 +16,4 @@ else
 {
 return (0);
 }
-
+}
