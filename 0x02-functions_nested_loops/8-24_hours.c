9db53d975cc85956a0bc0422feb796443fd157d9
Steve <evilco30@gmail.com>
2022-12-13 11:59:21 -0800

    8th

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (digit1 = 0; digit1 <= 2; digit1++)
 {
 for (digit2 = 0; digit2 <= 9; digit2++)
 {
-for (digit3 = 0; digit3 <= 9; digit3++)
+for (digit3 = 0; digit3 <= 5; digit3++)
 {
 for (digit4 = 0; digit4 <= 9; digit4++)
 {
