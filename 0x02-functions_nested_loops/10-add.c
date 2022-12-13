0271a5f0678ebc3f57942cf62fb5e11c73b5d7f5
Steve <evilco30@gmail.com>
2022-12-13 13:27:02 -0800

    Adding 9th

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @first: first number input
+ * @second: second number input
+ * Return: first + second
+ */
+int add(int first, int second)
+{
+return (first + second);
+}
