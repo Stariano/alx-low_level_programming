93d2a4de5cf7fa61bfe4f1edbf64da486c97fab6
Steve <evilco30@gmail.com>
2022-12-15 10:57:11 -0800

    Betty update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 {
 	int i, m;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (m = 0; m <= 14; m++)
+		for (m = 0; m < 14; m++)
 		{
 		_putchar(m);
 		_putchar('\n');
