33bfa8b181ffe38ace92af3c66e206e1ba463183
Steve <evilco30@gmail.com>
2022-12-15 12:14:26 -0800

    Betty

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,16 +8,15 @@
 void print_square(int size)
 {
 	int prnt, hw, columnlevel;
-	columnlevel = size - 1;
 	if (size <= 0)
 		_putchar('\n');
 	else
 		for (prnt = 0; prnt < size; prnt++)
 		{
-			for(hw = prnt - 1; hw < columnlevel; hw++)
+			for(hw = 0; hw < size; hw++)
 			{
 				_putchar('#');
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
 }
