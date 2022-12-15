26c2680110a4df21232aa2e1d45a088d718ef65d
Steve <evilco30@gmail.com>
2022-12-15 11:29:54 -0800

    Adding 8th

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a squareusing the character #.
+ * @size: The size of the square.
+ */
+
+void print_square(int size)
+{
+	int prnt;
+
+	if (size <= 0)
+		_putchar('\n')
+	else
+		for (prnt = 0; prnt <= size; prnt++)
+			_putchar('#')
+}
