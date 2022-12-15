9bd39a1b1d80972f932e6ba6a2f17636568c73cb
Steve <evilco30@gmail.com>
2022-12-15 12:19:14 -0800

    Solving betty issues

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_square - Prints a squareusing the character #.
+ * print_square - Prints a squareusing the character.
  * @size: The size of the square.
  */
 
 void print_square(int size)
 {
-	int prnt, hw, columnlevel;
-	if (size <= 0)
-		_putchar('\n');
-	else
-		for (prnt = 0; prnt < size; prnt++)
-		{
-			for(hw = 0; hw < size; hw++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+int prnt, hw, columnlevel;
+if (size <= 0)
+putcharr('\n');
+else
+for (prnt = 0; prnt < size; prnt++)
+{
+for (hw = 0 ; hw < size; hw++)
+{
+putcharr('#');
 }
+putcharr('\n');
+}
+}
+
