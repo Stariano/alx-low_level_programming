d9454ae5775bf9655ad17ab9d83788884860d5b2
Steve <evilco30@gmail.com>
2022-12-19 14:46:06 -0800

    First update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset to 98
+ * @n: Integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo will contain all of my work including pointers and tasks that ALX assigned it to us.
