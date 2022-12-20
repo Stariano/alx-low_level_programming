7e6cd46ad0afdd029c8932cc800e968edf10c530
Steve <evilco30@gmail.com>
2022-12-20 07:51:18 -0800

    Update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,23 +9,12 @@
 void print_rev(char *s)
 {
   int len = strlen(s);
-  int i = 0, j = 0;
-  char temp[100];
+  int i;
   
-
-  for (i = 0; i < len; i++)
-  {
-    for (j = 0; i < len; j++, i++)
+ for (i = len; i >= 0; i--)
     {
-      if (s[i] == ' ' || s[i] == '.')
+        if (s[i] == ' ' || s[i] == '.')
         break;
-      temp[j] = s[i];
+        printf("%c", s[i]);
     }
-    while (j > 0)
-    {
-      j--;
-      s[i - j - 1] = temp[j];
-    }
-  }
-  printf("%s\n", s);
 }
