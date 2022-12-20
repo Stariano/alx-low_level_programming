c371eb8bcc73c44101fc74d877432bf139b77df7
Steve <evilco30@gmail.com>
2022-12-20 08:18:51 -0800

    Update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,13 @@
 
 void rev_string(char *s)
 {
-        int length = strlen(s);
-        int i;
+int len = strlen(s);
+int i;
 
-        for (i = length - 1; i >= 0; i--) {
-                if (s[i] == ' ') {
-                        s[i] = '\0';
-                        printf("%s ", &(s[i]) + 1);
-                }
-        }
-    printf("%s", s);
+for (i = len; i >= 0; i--)
+{
+                _putchar(s[i]);
 }
+        _putchar('\n');
+}
+
