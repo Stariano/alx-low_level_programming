9d700b03e5b9dd402f7f398c5c1474af669625d1
Steve <evilco30@gmail.com>
2022-12-23 05:48:38 -0800

    Adding Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,30 +9,42 @@
  */
 
 char *cap_string(char *s)
-{
-int i, length;
-length = strlen(s);
+	{
+	int i, length;
 
-for (i = 0; i < length; i++)
-{
+	length = strlen(s);
 
-if (s[i] == ' ' || s[i] == '.' || s[i] == '\t')
-{
-++i;
-if (s[i] >= 97 && s[i] <= 122)
-{
-s[i] = s[i] - 32;
-}
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == '!')
+		{
+			++i;
+			if (s[i] == ' ')
+			++i;
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] = s[i] - 32;
+			}
+		}
 
-}
-if (s[i] == '\n' || s[i] == '6' || s[i] == '!')
-{
-++i;
-if (s[i] >= 97 && s[i] <= 122)
-{
-s[i] = s[i] - 32;
-}
-}
-}
-return (s);
+		if (s[i] == ' ' || s[i] == '.' || s[i] == '\t')
+		{
+			++i;
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] = s[i] - 32;
+			}
+
+		}
+		if (s[i] == '\n' || s[i] == '6' || s[i] == '!')
+		{
+			++i;
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+
+	}
+	return (s);
 }
