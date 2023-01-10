7d2ed39e3079ad96c1e5dfa0b1a95b2d9d02705f
Steve <evilco30@gmail.com>
2023-01-10 09:03:59 -0800

    All tasks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+
 /**
- * main - check the code for Holberton School students.
- * @argv: The character to print
- * @argc: The character to print
- * Return: Always 0.
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	argv = argv;
+	int i;
 
-	printf("%d\n", argc);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
 
 	return (0);
 }
