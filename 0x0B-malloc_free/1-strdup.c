fa1d98f31cf3b424d69161a1356e9d89709e3f9f
Steve <evilco30@gmail.com>
2023-01-11 12:07:31 -0800

    Secind task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Entry point
+ *@str: string we need to duplicate
+ * Return: a pointer to the duplicated string or NULL
+ */
+char *_strdup(char *str)
+{
+	char *strnew = NULL;
+	unsigned int i;
+	int len = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+
+	strnew = malloc(len * sizeof(char));
+
+	for (i = 0; i < len; i++)
+	{
+		strnew[i] = str[i];
+	}
+
+	return (strnew);
+
+}
+
