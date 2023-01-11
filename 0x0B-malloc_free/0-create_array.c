0a54c966d9aa55f67174797f1f087496502a650f
Steve <evilco30@gmail.com>
2023-01-11 10:24:29 -0800

    First task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,6 +20,11 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(size * sizeof(char));
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
