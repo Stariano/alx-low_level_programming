3e0e88f025f8eb26f7381543e423441de501d045
Steve <evilco30@gmail.com>
2023-01-19 10:07:47 -0800

    First task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all - Entry Point
+ * @n: variadic inputs
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i;
+	int result = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	
+	for (i = 0; i < n; i++)
+	{	
+		result = result + va_arg(ap, const unsigned int);
+	}
+
+	va_end(ap);
+	
+	return (result);
+}
