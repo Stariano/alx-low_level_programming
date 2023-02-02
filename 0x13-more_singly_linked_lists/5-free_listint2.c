a621a38f02eeee442f8ef0c1849ebb15bd19b7c1
Steve <evilco30@gmail.com>
2023-02-01 16:28:38 -0800

    Hot fix in task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,15 +11,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
-	
+
 	if (head == NULL)
 		return;
-	
-	while(temp != NULL)
+
+	while (temp != NULL)
 	{
 		temp = temp->next;
-		free(*head);
 		*head = temp;
+		free(*head);
 	}
 
 	head = NULL;
