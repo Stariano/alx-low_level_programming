532fac12c6e0cb078e2263f0a8a79bc6ae99453b
Steve <evilco30@gmail.com>
2023-02-01 16:24:34 -0800

    Hot fix in task 5

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,14 +4,15 @@
  * free_listint - Frees a listint_t list.
  * @head: A pointer to the head of the listint_t list to be freed.
  */
+
 void free_listint(listint_t *head)
 {
-	listint_t temp = *head;
+	listint_t *temp = head;
 
 	while (temp != NULL)
 	{
 		temp = temp->next;
-		free(*head);
-		*head = temp;
+		free(head);
+		head = temp;
 	}
 }
