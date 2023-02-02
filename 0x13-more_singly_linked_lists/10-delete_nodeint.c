c0a354e8bd5669b90a2c57953d1b17c22f342fd1
Steve <evilco30@gmail.com>
2023-02-01 16:57:46 -0800

    Hot fix in task 5

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,14 +15,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 
 	if (temp == NULL)
-		return (-1)
+		return (-1);
 
 	while (index > 0)
 	{
 		if (temp == NULL || temp->next == NULL)
 			return (-1);
 		temp = temp->next;
-		idx--;
+		index--;
 	}
 	*head = temp;
 	free(*head);
