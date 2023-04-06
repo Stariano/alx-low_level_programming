95920eb23f4438c5f36817dec13b62df0bd2cfa7
Steve <evilco30@gmail.com>
2023-04-06 11:15:36 -0700

    Minor

--- 0x1A-hash_tables/4-hash_table_get-0.c
+++ 0x1A-hash_tables/4-hash_table_get-0.c
@@ -7,26 +7,26 @@
  *
  * Return: the value associated with the key, or NULL if key can't be found.
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *node;
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *item = ht->array[index];
 
 	if (ht == NULL)
 		return (NULL);
 	if (key == NULL)
 		return (NULL);
-	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
 	if (strcmp(ht->array[index]->key, key) == 0)
 		return (ht->array[index]->value);
-	node = ht->array[index];
-	while (node != NULL)
+
+	while (item != NULL)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
 	}
 	return (NULL);
 }
